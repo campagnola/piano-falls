175817790d215d8b67734f9b128fd015d1606084
Luke Campagnola <luke.campagnola@gmail.com>
2022-06-05 14:32:50 -0700

    add initial midi support

--- main.js
+++ main.js
@@ -2,17 +2,81 @@ import { SVG } from './node_modules/@svgdotjs/svg.js/dist/svg.esm.js'
 
 
 var keyProps = []
-var keyboard
 var rootSvg
+var keyboard
+var waterfall
 
 export function init() {
     rootSvg = $("svg")[0]
-    keyboard = SVG().addTo(rootSvg)
+    keyboard = makeKeyboard()
+    keyboard.addTo(rootSvg)
+
+    waterfall = SVG().addTo(rootSvg).x(0).y(0)
+    waterfall.attr({'viewBox': '0 0 88 10', 'preserveAspectRatio': 'none'})
+    waterfall.rect(88, 10).x(0).y(0).attr({'stroke': '#0000', 'fill': '#000'})
+
+    $(window).resize(onResize)
+    onResize()
+
+    navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure)
+}
+
+
+function onMIDISuccess(midiAccess) {
+    for (var input of midiAccess.inputs.values()) {
+        input.onmidimessage = onMIDIMessage
+        console.log(input)
+    }
+}
+
+function onMIDIFailure() {
+    console.log('Could not access your MIDI devices.')
+}
+
+function onMIDIMessage(midiMessage) {
+    console.log(midiMessage)
+    switch( midiMessage.data[0] ) {      
+        case 144: {  // note on
+            const keyId = midiMessage.data[1] - 21
+            pressKey(keyId)
+            break
+        }
+        case 128: { // note off
+            const keyId = midiMessage.data[1] - 21
+            releaseKey(keyId)
+            break
+        }
+    }
+}
+
+
+function pressKey(keyId) {
+    key = keyProps[keyId]
+    key['svg'].attr('fill', '#578')
+}
+
+
+function releaseKey(keyId) {
+    key = keyProps[keyId]
+    key['svg'].attr('fill', key['color'])
+}
+
+
+function onResize() {
+    const width = $(window).width()
+    const keyboardHeight = 0.114 * width
+    $(rootSvg).attr({'height': $(window).height()})
+    keyboard.attr({'width': width, 'height': keyboardHeight, 'y': $(window).height() - keyboardHeight})
+
+    waterfall.attr({'width': width, 'height': $(window).height() - keyboardHeight})
+}
+
+
+function makeKeyboard() {
+    keyboard = SVG()
     const width = $(window).width()
     const height = 0.114 * width
     keyboard.attr({'name': 'keyboard_svg', 'viewBox': '0 0 88 10'})
-    onResize()
-    $(window).resize(onResize)
 
     var whiteKeyWidth = 88 / 52
     var blackKeyWidth = 88 * (7 / 52) / 12
@@ -30,6 +94,7 @@ export function init() {
             'color': isBlackKey ? '#000' : '#fff',
             'keyId': keyId,
             'subIndex': isBlackKey ? blackKeyIndex : whiteKeyIndex,
+            'pressed': false,
         }
         keyProps.push(key)
         if( isBlackKey ) {
@@ -54,13 +119,6 @@ export function init() {
         })
         key['svg'] = rect
     }
-    keyboard.line(0, 10, 88, 0).attr({'stroke-width': 1, 'color': '#f00', 'vector-effect': 'non-scaling-stroke',})
-}
-
-
-function onResize() {
-    const width = $(window).width()
-    const height = 0.114 * width
-    $(rootSvg).attr({'height': $(window).height()})
-    keyboard.attr({'width': width, 'height': height, 'y': $(window).height() - height})
+    keyboard.line(0, 0, 88, 0).attr({'stroke': '#024', 'stroke-width': 6, 'vector-effect': 'non-scaling-stroke'})
+    return keyboard
 }
